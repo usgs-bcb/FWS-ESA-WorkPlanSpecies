f37c8e6e5e82f1902291bc0df4446dcef5009c1d
Nathan Tarr <nmtarr@ncsu.edu>
2018-09-07 16:01:28 -0400

    Started coding deliverables for FWS.  Species-state_protection_tables.py is my working script and the similar notebook is just a duplicate for demonstration purposes.

--- species-state_protection_tables.py
+++ species-state_protection_tables.py
@@ -1,27 +1,140 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
+nmtarr 9/7/2018
 
-This is a temporary script file.
+
 """
-from IPython.display import display
-import pandas as pd
+# import config file with username and password
 import sys
 sys.path.append("T:/BCB/")
 import BISdbConfig as config
 
 # pybis needs environment variables to connect to the database.
-#import os
-#os.environ['DB_USERNAME']=config.user
-#os.environ['DB_PASSWORD']=config.password
-#os.environ['MONGODB_SERVER']="54.91.95.139"
-#os.environ['DB_DATABASE']="bis"
+import os
+os.environ['DB_USERNAME']=config.user
+os.environ['DB_PASSWORD']=config.password
+os.environ['MONGODB_SERVER']="54.91.95.139"
+os.environ['DB_DATABASE']="bis"
+
 from pybis import db
-
-
-
-
+from IPython.display import display
+import pandas as pd
+pd.set_option('display.width', 1000)
+pd.set_option('display.max_columns', 12)
+pd.set_option('display.max_rows', 100)
+import us
+import pprint
 
 bisDB = db.Db.connect_mongodb("bis")
 esaWPSpecies = bisDB["FWS_Work_Plan_Species"]
-print(esaWPSpecies)
+
+
+## Species names
+#for record in esaWPSpecies.find({"Synthesis.Unique Scientific Names.1":{"$exists":True}}):
+#    print ("Submitted Name:", record["Submitted Data"]["Scientific Name"])
+#    try:
+#        print ("ECOS Name:", record["ECOS Scrape"]["Scientific Name"])
+#        print ("ECOS TSN:", record["ECOS Scrape"]["TSN"])
+#    except:
+#        pass
+#    try:
+#        print ("TESS Name:", record["TESS"]["SCINAME"])
+#    except:
+#        pass
+#    for itisRecord in record["ITIS"]:
+#        print ("ITIS:", itisRecord["nameWInd"], itisRecord["usage"], itisRecord["tsn"])
+#    print ("----------")
+    
+sp = "Ambystoma barbouri"
+
+
+# Build dataframe to fill out
+columns=["FWS_range(y/n)", "BISON_occurrences", "SGCN_2015(y/n)", 
+         "SGCN_2005(y/n)", "GAP_habitat(%)", "status1(%)", "status2(%)", 
+         "status3(%)", "status4(%)"]
+df0 = pd.DataFrame(index=[str(x) for x in us.STATES], columns=columns)
+df0.index.name="State"
+
+##########################################################################  FWS
+# Fill out ECOS column with whether or not the ECOS account lists the state
+try:
+    FWS = esaWPSpecies.find_one({"Submitted Data.Scientific Name": sp})['FWS Range']
+    FWS_states = FWS['US State List']
+    for x in FWS_states:
+        for y in FWS['US States']:
+            if x == list(y.keys())[0]:
+                state_name = y[x]['name'].strip()
+                df0.loc[state_name, 'FWS_range(y/n)'] = 1
+except:
+    print("No FWS range")
+    
+########################################################################  BISON
+# How many occurrences are in each state?
+try:
+    bison = esaWPSpecies.find_one({"Submitted Data.Scientific Name": sp})['BISON']
+    bison_states = bison["US State Occurrences"]
+    for state in bison_states:
+        state_name = list(state.keys())[0]
+        df0.loc[state_name, "BISON_occurrences"]=int(state[state_name])
+except:
+    print("No BISON")
+
+#########################################################################  SGCN 
+# Which states declared it SGCN?
+try:
+    SGCN = esaWPSpecies.find_one({"Submitted Data.Scientific Name": sp})['SGCN']
+    for x in SGCN['State List Summary']:
+        if list(x.keys())[0] == 2005:
+            statess = x['2005']['States']
+            for z in statess:
+                df0.loc[z, 'SGCN_2005(y/n)'] = 1
+        elif list(x.keys())[0] == 2015:
+            statess = x['2015']['States']
+            for z in statess:
+                df0.loc[z, 'SGCN_2015(y/n)'] = 1  
+except:
+    print("No SGCN")   
+
+print(df0)   
+
+df0.to_csv("T:/temp/FWS-ESA_temp.csv")           
+#            
+###################################################################  GAP habitat 
+##
+#for record in esaWPSpecies.find({"GAP":{"$exists":True}}):
+#    pprint.pprint(record)
+#    
+#    
+#    
+#try:
+#    
+#    GAP = esaWPSpecies.find_one({"GAP":{"$exists":True}},{"Submitted Data":1,"GAP":1})
+#    
+#except:
+#    print("No GAP")
+#    
+#    
+#    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
