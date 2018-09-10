194906a96ea3a378ee612539f5a9880b71023fa3
Nathan Tarr <nmtarr@ncsu.edu>
2018-09-10 16:39:57 -0400

    Added code to plot maps

--- Species-state summaries.py
+++ Species-state summaries.py
@@ -6,8 +6,11 @@ Description: Script used in developing State range summaries.ipynb.
 
 """
 #from IPython.display import display as display1
+from matplotlib.colors import ListedColormap    
 import us
 import pandas as pd
+import geopandas as gpd
+import matplotlib.pyplot as plt
 pd.set_option('display.width', 1000)
 pd.set_option('display.max_columns', 12)
 pd.set_option('display.max_rows', 100)
@@ -36,7 +39,7 @@ esaWPSpecies = bisDB["FWS_Work_Plan_Species"]
 ## Get a list of species names to loop on.
 spp = [record["Submitted Data"]["Scientific Name"] for record in esaWPSpecies.find()]
 
-for sp in spp[:3]:
+for sp in spp[:1]:
     # Build dataframe to fill out
     columns=[]
     df0 = pd.DataFrame(index=[str(x) for x in us.STATES], columns=columns)
@@ -70,7 +73,7 @@ for sp in spp[:3]:
         BISON_states = Synth['States with BISON Occurrence Data']
         for x in BISON_states:
             state_name = x.strip()
-            df0.loc[state_name, 'Has_BISON_Data(y/n)'] = 1
+            df0.loc[state_name, 'has_BISON_Data(y/n)'] = 1
     except:
         print("Exception -- BISON state list")
     
@@ -99,12 +102,72 @@ for sp in spp[:3]:
     except:
         print('Exception -- GAP states, species.')
     
-    #################################################################  Clean up\
+    #####################################################################  Clean up
     df1 = df0.fillna(0).astype(int)
     # get rid of non-CONUS states
     lowers = [str(x) for x in us.STATES_CONTINENTAL]
     print(sp + ' state range summary')
     df2 = df1.filter(items=lowers, axis=0)
     display(df2)
-    print('\n\n')
+    print('\n\n') 
     
+    ########################################################  Add comparison fields
+    # Compare SGCN 2015 to FWS
+    codeDict = {(1,1): 'SGCN & FWS', (1,0): 'SGCN only', (0,1): 'FWS only', (0,0): 'Neither'}
+    df2['SGCN2015-FWS'] = list(zip(df2['SGCN_2015(y/n)'], df2['FWS_range(y/n)']))
+    df2['SGCN2015-FWS'] = [codeDict[x] for x in df2['SGCN2015-FWS']]
+    
+    
+    
+    
+    ####################################################################  Plot
+    # SGCN 2015 vs. FWS
+    # Read in state geojson from ScienceBase
+    statesGDF = gpd.read_file("T:/Data/state_boundaries.geojson") 
+    # Join states geodataframe with species dataframe
+    statesGDF2 = pd.merge(df2, statesGDF, how='inner', 
+                          left_on=df2.index, right_on=statesGDF.STATE_NAME)
+    statesGDF3 = gpd.GeoDataFrame(statesGDF2)
+    statesGDF3 = statesGDF3[statesGDF3.STATE_NAME != 'Alaska']
+           
+    # Plot
+    fig, ax = plt.subplots(1, figsize=(13, 7))
+    statesGDF3.plot(ax=ax, column="SGCN2015-FWS", legend=True, 
+                    cmap=ListedColormap(['yellow', 'grey', 'green', 'blue']),
+                    categorical=True, linewidth=0.5, edgecolor='0.9')
+    ax.set_ylim(23, 50)
+    ax.set_xlim(-126, -66)
+    ax.set_axis_off()
+    plt.axis('equal')
+    fig.suptitle(sp + ' SGCN 2015 and FWS ranges', fontsize=18)
+    
+    
+    # GAP Habitat
+    # Convert column values
+    statesGDF3['has_GAP_habitat(y/n)'] = [{0: "No", 1: "Yes"}[x] for x in statesGDF3['has_GAP_habitat(y/n)']]
+    # Plot
+    fig1, ax1 = plt.subplots(1, figsize=(13, 7))
+    statesGDF3.plot(ax=ax1, column="has_GAP_habitat(y/n)", legend=True, 
+                    cmap=ListedColormap(['grey', 'green']),
+                    categorical=True, linewidth=0.5, edgecolor='0.9')
+    ax1.set_ylim(23, 50)
+    ax1.set_xlim(-126, -66)
+    ax1.set_axis_off()
+    plt.axis('equal')
+    fig1.suptitle(sp + " GAP range", fontsize=18)
+    
+    
+    # BISON
+    # Convert column values
+    statesGDF3['has_BISON_data(y/n)'] = [{0: "No", 1: "Yes"}[x] for x in statesGDF3['has_BISON_Data(y/n)']]
+    # Plot
+    fig2, ax2 = plt.subplots(1, figsize=(13, 7))
+    statesGDF3.plot(ax=ax2, column="has_BISON_data(y/n)", legend=True, 
+                    cmap=ListedColormap(['grey', 'blue']),
+                    categorical=True, linewidth=0.5, edgecolor='0.9')
+    ax2.set_ylim(23, 50)
+    ax2.set_xlim(-126, -66)
+    ax2.set_axis_off()
+    plt.axis('equal')
+    fig2.suptitle(sp + " BISON range", fontsize=18)
+
